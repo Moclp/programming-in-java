efb5ed1a10d350711b0840c86744d109c178928f
moclp <moise.clipal@efrei.net>
2023-10-13 07:36:25 +0200

    javadoc inteface of QueueOfInts.java

--- lab03/src/main/java/agh/ii/prinjava/lab03/exc03_01/QueueOfInts.java
+++ lab03/src/main/java/agh/ii/prinjava/lab03/exc03_01/QueueOfInts.java
@@ -1,15 +1,45 @@
 package agh.ii.prinjava.lab03.exc03_01;
 
+/**
+ * This interface represents a queue of integers.
+ */
 public interface QueueOfInts {
+    /**
+     * Adds an integer to the rear of the queue.
+     *
+     * @param x The integer to be added to the queue.
+     */
     void enqueue(int x);
 
-    int dequeue();
+    /**
+     * Removes and returns the integer from the front of the queue.
+     *
+     * @return The integer removed from the queue.
+     * @throws EmptyQueueException If the queue is empty when trying to dequeue.
+     */
+    int dequeue() throws EmptyQueueException;
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return {@code true} if the queue is empty, {@code false} otherwise.
+     */
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /**
+     * Returns the number of elements in the queue.
+     *
+     * @return The number of elements in the queue.
+     */
     int numOfElems();
 
+    /**
+     * Returns the integer at the front of the queue without removing it.
+     *
+     * @return The integer at the front of the queue.
+     * @throws EmptyQueueException If the queue is empty when trying to peek.
+     */
     int peek();
 }
