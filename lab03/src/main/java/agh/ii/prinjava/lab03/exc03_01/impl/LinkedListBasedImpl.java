2623ecf16954f2939ccbcf006258b4659c042536
moclp <moise.clipal@efrei.net>
2023-10-13 07:38:30 +0200

    javadoc inteface of LinkedListBasedImpl.java

--- lab03/src/main/java/agh/ii/prinjava/lab03/exc03_01/impl/LinkedListBasedImpl.java
+++ lab03/src/main/java/agh/ii/prinjava/lab03/exc03_01/impl/LinkedListBasedImpl.java
@@ -4,6 +4,9 @@ import agh.ii.prinjava.lab03.exc03_01.QueueOfInts;
 
 import java.io.*;
 
+/**
+ * This class provides an implementation of a queue of integers using a linked list.
+ */
 public class LinkedListBasedImpl implements QueueOfInts, Serializable {
 
     private static class Node implements Serializable {
@@ -19,10 +22,18 @@ public class LinkedListBasedImpl implements QueueOfInts, Serializable {
     private Node front; // Front of the queue
     private Node rear;  // Rear of the queue
 
+    /**
+     * Constructs an empty queue.
+     */
     public LinkedListBasedImpl() {
         this.front = this.rear = null;
     }
 
+    /**
+     * Adds an integer to the rear of the queue.
+     *
+     * @param x The integer to be added to the queue.
+     */
     @Override
     public void enqueue(int x) {
         Node newNode = new Node(x);
@@ -35,6 +46,12 @@ public class LinkedListBasedImpl implements QueueOfInts, Serializable {
         }
     }
 
+    /**
+     * Removes and returns the integer from the front of the queue.
+     *
+     * @return The integer removed from the queue.
+     * @throws EmptyQueueException If the queue is empty when trying to dequeue.
+     */
     @Override
     public int dequeue() throws EmptyQueueException {
         if (isEmpty()) {
@@ -51,6 +68,21 @@ public class LinkedListBasedImpl implements QueueOfInts, Serializable {
         return removedValue;
     }
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return {@code true} if the queue is empty, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    /**
+     * Returns the number of elements in the queue.
+     *
+     * @return The number of elements in the queue.
+     */
     @Override
     public int numOfElems() {
         int count = 0;
@@ -62,6 +94,12 @@ public class LinkedListBasedImpl implements QueueOfInts, Serializable {
         return count;
     }
 
+    /**
+     * Returns the integer at the front of the queue without removing it.
+     *
+     * @return The integer at the front of the queue.
+     * @throws EmptyQueueException If the queue is empty when trying to peek.
+     */
     @Override
     public int peek() throws EmptyQueueException {
         if (isEmpty()) {
@@ -70,17 +108,20 @@ public class LinkedListBasedImpl implements QueueOfInts, Serializable {
         return front.value;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return front == null;
-    }
-
+    /**
+     * This is a custom checked exception class for handling cases when the queue is empty.
+     */
     public class EmptyQueueException extends Exception {
         public EmptyQueueException(String message) {
             super(message);
         }
     }
 
+    /**
+     * Serializes the queue to a file.
+     *
+     * @param filename The name of the file to save the serialized data.
+     */
     public void serialize(String filename) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
             out.writeObject(this);
@@ -89,8 +130,14 @@ public class LinkedListBasedImpl implements QueueOfInts, Serializable {
         }
     }
 
+    /**
+     * Deserializes a saved queue from a file and returns a new instance of LinkedListBasedImpl.
+     *
+     * @param filename The name of the file containing the serialized data.
+     * @return A new instance of LinkedListBasedImpl with the deserialized data, or null in case of errors.
+     */
     public static LinkedListBasedImpl deserialize(String filename) {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename)) ){
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
             return (LinkedListBasedImpl) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
